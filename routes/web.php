b1c5e66b4089f27d9f13b5e77392f47c24d66d3a
Ekya1411 <ekyamuhammad@gmail.com>
2025-02-19 09:18:31 +0700

    praktikum1 js1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '
+    Nama : Ekya <br>
+    Nim : 2341720111';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan id' . $id;
+});
+
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama saya ' . $name;
+// });
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
 });
